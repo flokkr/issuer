b879a07ba42fd0ca71c451a2a3377323ee2ca795
Márton Elek <elek@apache.org>
2019-02-25 16:19:13 +0100

    fix type

--- bash/keytab.sh
+++ bash/keytab.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 keytab(){
-  echo "Generiting keytab"
+  echo "Generating keytab"
   kadmin -kt /tmp/admin.keytab -p admin/admin -q "addprinc -randkey $1"
   kadmin -kt /tmp/admin.keytab -p admin/admin -q "ktadd -k $2 $1"
 }
